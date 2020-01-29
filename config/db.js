4b3b4b7649cec35543742a4f4d9f872c83bbec18
lanretipmis <54453873+lanretipmis@users.noreply.github.com>
2020-01-29 21:13:31 +0300

    --

--- config/db.js
+++ config/db.js
@@ -2,18 +2,20 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-    mongoose
-    .connect(db,{
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    })
-    .then(() => console.log('mongoDB Connected'))
-    .catch((err) => {
+const connectDB = async () => {
+    try {
+        await mongoose
+            .connect(db,{
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useFindAndModify: false
+            });
+        console.log('mongoDB Connected...')
+    } catch (err) {
         console.error(err.message);
         process.exit(1)
-    })
+    }
+
 };
 
 module.exports = connectDB;
