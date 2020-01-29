c86d64e9c79d38f1d30a49a7fa68d80ac433f14f
lanretipmis <54453873+lanretipmis@users.noreply.github.com>
2020-01-29 20:35:59 +0300

    -

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = () => {
-    mongoose.connect(db,{
+    mongoose
+    .connect(db,{
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false
