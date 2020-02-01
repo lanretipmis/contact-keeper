68831d7b357bc0cfb1a58a112a595378c9474fd6
lanretipmis <54453873+lanretipmis@users.noreply.github.com>
2020-02-01 16:39:02 +0300

    serach

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -3,13 +3,15 @@ import PropTypes from "prop-types";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactItem = ({ contact }) => {
-  const { id, name, email, phone, type } = contact;
   const contactContext = useContext(ContactContext);
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
-  const deleteContact = e => {
-    e.preventDefault();
-    contactContext.deleteContact();
-  }
+  const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+    clearCurrent();
+  };
 
   return (
     <div className="card bg-light">
@@ -30,7 +32,7 @@ const ContactItem = ({ contact }) => {
           <li>
             <i className="fas fa-envelope-open"></i>
             {email}
-          </li>
+           </li>
         )}
         {phone && (
           <li>
@@ -40,8 +42,13 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <p>
-        <button className="btn btn-dark btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm" onClick={deleteContact}>Delete</button>
+        <button
+          className="btn btn-dark btn-sm"
+          onClick={() => setCurrent(contact)}>Edit
+        </button>
+        <button className="btn btn-danger btn-sm" onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   );
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -32,12 +32,14 @@ const ContactState = props => {
       },
       {
         id: 3,
-        name: "harry White",
+        name: "Harry White",
         email: "harry@gmail.com",
         phone: "523-2112-3333",
         type: "professional"
       }
-    ]
+    ],
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -47,25 +49,51 @@ const ContactState = props => {
     contact.id = uuid.v4();
     dispatch({ type: ADD_CONTACT, payload: contact });
   };
+
   //delete contact
-  const deleteContact = () => {
-    console.log('DELETEE')
-  }
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
   //set current contact
+  const setCurrent = contact => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   //clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   //update contact
+  const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   //filter contact
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   //clear filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
+
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
