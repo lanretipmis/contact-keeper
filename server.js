1fcf306973d9be44eac9e9ac293f657791d4f772
lanretipmis <54453873+lanretipmis@users.noreply.github.com>
2020-02-05 16:07:58 +0300

    --

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require("path");
 const app = express();
 
 //Connect DataBase
@@ -9,13 +9,19 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcom to the contact keeper API" })
-);
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//serve static assets in prod
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 8080;
 
